d70ff8ddaaded5c0938205adb69768669ca709fe
said <madi888364@gmail.com>
2023-10-29 23:05:17 +0300

    task1

--- numbers.js
+++ numbers.js
@@ -1,1 +1,12 @@
+function calculateDistance(x1, x2) {
+  const result = Math.abs(x1 - x2);
+  return result;
+}
 
+console.log(calculateDistance(0, 5));
+
+function calculateSegmentProduct() {
+}
+export {
+  calculateDistance, calculateSegmentProduct,
+};
